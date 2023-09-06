f806ae92bc86a536964a0f1cf7e78079a00c8c13
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2023-09-06 22:10:18 +0300

    + message.go

---
+++ backend/internal/models/message.go
@@ -0,0 +1,20 @@
+package models
+
+import "time"
+
+// Message represents a chat message in the chat application.
+type Message struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Username  string    `json:"username"`
+	RoomID    int64     `json:"room_id"`
+	Content   string    `json:"content"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// MessageInput represents the input data for creating a message.
+type MessageInput struct {
+	UserID  int64  `json:"user_id"`
+	RoomID  int64  `json:"room_id"`
+	Content string `json:"content"`
+}
